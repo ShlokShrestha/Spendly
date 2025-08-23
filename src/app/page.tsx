b30f7afca03b3aeff6332bf955bcc6673e8517df
shlokshrestha <shlokstha99@gmail.com>
2025-08-23 23:03:37 +0545

    change in login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Login from "./(Auth)/login/page";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <>
-      <Login />
+      Hello Test
+      <Button onClick={() => router.push("/login")}>Login</Button>
     </>
   );
 }
